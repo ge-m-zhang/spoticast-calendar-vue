2cac945388d34848fc71c78c9848f59316328f01
gezhang <ge.melody.zhang@gmail.com>
2025-04-08 20:42:27 -0400

    simple UI for testing search

---
+++ src/components/SearchSection/SearchSection.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { useSearchStore } from '@/stores/searchStore'
+
+const searchStore = useSearchStore()
+
+// test searchStore
+function handleSearch(query: string) {
+  searchStore.performSearch(query)
+}
+</script>
+
+<template>
+  <h1>Search Section</h1>
+  <button @click="handleSearch('technology')">Search</button>
+</template>
